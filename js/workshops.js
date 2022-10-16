47f75e7ed0d740c0870a2eed53f6a7b36f4d7969
Huzaif Akhtar <64411465+huzaifakhtar@users.noreply.github.com>
2022-10-16 17:01:01 +0530

    update 16-10 huzaifakhtar

---
+++ js/workshops.js
@@ -0,0 +1,42 @@
+function workshopDisplay(workshop) {
+    return `
+        <li>
+            <div class="ws-box" style="background-image: url('${workshop.image}');">
+            <a href="${workshop.link}" class="wslink">
+                <div class="ws-info-present">
+                    <h3>${workshop.workshopName}</h3>
+                    <p>${workshop.about}</p>
+                    <h4>📅 ${workshop.date} | ⏲️ ${workshop.time} </h4>
+                </div>
+            </a>
+            </div>
+        </li>
+        `;
+    }
+  
+    document.getElementById("upcoming").innerHTML = `
+    ${workshopData.map(workshopDisplay).join("")}
+    <p>There are ${workshopData.length} upcoming workshops.</p>
+`;
+
+//Past Workshops
+
+function pastWorkshops(workshop) {
+    return `
+        <li>
+            <div class="ws-box" style="background-image: url('${workshop.image}');">
+            <a href="${workshop.link}" class="wslink">
+                <div class="ws-info">
+                    <h3>${workshop.workshopName}</h3>
+                    <p>${workshop.about}</p>
+                </div>
+            </a>
+            </div>
+        </li>
+        `;
+    }
+  
+  document.getElementById("past").innerHTML = `
+  ${pastWorkshopData.map(pastWorkshops).join("")}
+  <p>There are ${pastWorkshopData.length} workshops.</p>
+`;
