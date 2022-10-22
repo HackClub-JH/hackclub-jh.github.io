3ae7559c0dc9d777f004e6e34ad47b280d3281f8
Najeeb Ahmad <105259193+NajeebAhmad00@users.noreply.github.com>
2022-10-22 05:09:09 -0700

    Update workshops_data.js

--- js/workshops_data.js
+++ js/workshops_data.js
@@ -5,10 +5,7 @@ const workshopData = [
     //     link: "https://iter.hackclub.com/workshops", //enter the link of the workshop
     //     image: "https://images.unsplash.com/photo-1542626991-cbc4e32524cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya3Nob3B8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", //give the image (make sure to host it before adding)
     // },
-    
-    
-
-];
+]
 
 //Past
 
@@ -68,4 +65,4 @@ const pastWorkshopData = [
         image: "./img/events/inaugration.jpg", //give the image (make sure to host it before adding)
     },
     
-];
+]
